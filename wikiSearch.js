fe7ccd7f0899f17ec951822ff46bfc2f1d702bec
Ian <ian (at) ianrobinson.net>
2017-11-22 20:39:06 -0800

    Add wiki api connect, but data is poorly displayed

--- wikiSearch.js
+++ wikiSearch.js
@@ -1,9 +1,21 @@
 $(document).ready(function() {
-  var didWeGetIt = "no";
-  
+    
   $('.input').on('keyup', function() {
-    var search = $('.input').val();
-    $('.searchResults').text(search);
+    var term = $('.input').val();
+    var remoteUrlWithOrigin = "http://en.wikipedia.org/w/api.php?action=parse&format=json&prop=text&section=0&page=" + term + "&callback=?"
+    
+    $.ajax( {
+        url: remoteUrlWithOrigin,
+        data: term,
+        dataType: 'json',
+        type: 'GET',
+        headers: { 'Api-User-Agent': 'Example/1.0' },
+        success: function(data) {
+          var str = data;
+          console.log(str, "<-str");
+          $('.searchResults').text(str);
+        }
+    } );
   });
 
   $('.search').on('click', function() {
