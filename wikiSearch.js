429a85f9872812d8c5370e00a43a23b0fc3bf3cb
Ian <ian (at) ianrobinson.net>
2017-11-27 18:28:30 -0800

    API call successful - current app feeds rapid answers to exact queries via wikipedia API

--- wikiSearch.js
+++ wikiSearch.js
@@ -2,18 +2,22 @@ $(document).ready(function() {
     
   $('.input').on('keyup', function() {
     var term = $('.input').val();
-    var remoteUrlWithOrigin = "http://en.wikipedia.org/w/api.php?action=parse&format=json&prop=text&section=0&page=" + term + "&callback=?"
+    var remoteUrlWithOrigin = "https://en.wikipedia.org/w/api.php?action=opensearch&search=" + term + "&format=json&callback=?";
     
     $.ajax( {
         url: remoteUrlWithOrigin,
-        data: term,
-        dataType: 'json',
         type: 'GET',
-        headers: { 'Api-User-Agent': 'Example/1.0' },
+        async: false,
+        dataType: 'json',
+        
         success: function(data) {
-          var str = data;
-          console.log(str, "<-str");
-          $('.searchResults').text(str);
+          console.log(data)
+          $('.searchResults').text(data[1][0]);
+          $('.searchResults').text(data[2][0]);
+        },
+        
+        error: function(errorMessage) {
+          alert("AJAX function error by Ian");
         }
     } );
   });
